a0841d49a3cb427c7f6135b7786e4f5c2bc502bd
kalkidan behailu <kalkidanbehailu55@gmail.com>
2024-12-24 11:26:10 +0300

    change setting statlement

--- promodoro-extention/src/components/SettingsModal.jsx
+++ promodoro-extention/src/components/SettingsModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const SettingsModal = ({ tabsData, onSave, onClose }) => {
+const SettingsModal = ({ tabsData, onSave, onClose, isDarkMode }) => {
   const [updatedTabs, setUpdatedTabs] = useState([...tabsData]);
 
   const handleChange = (index, field, value) => {
@@ -14,8 +14,14 @@ const SettingsModal = ({ tabsData, onSave, onClose }) => {
   };
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="p-6 rounded-lg shadow-lg bg-white max-w-md w-full">
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 pr-72">
+      <div
+        className={`p-6 rounded-lg shadow-lg ${
+          isDarkMode
+            ? "bg-blue-900 text-white shadow-xl shadow-blue-900"
+            : "bg-slate-200 text-black shadow-lg shadow-gray-400"
+        } w-68`}
+      >
         <h2 className="text-2xl font-bold mb-4">Settings</h2>
         {updatedTabs.map((tab, index) => (
           <div key={tab.value} className="mb-4">
@@ -26,20 +32,30 @@ const SettingsModal = ({ tabsData, onSave, onClose }) => {
               type="number"
               value={tab.duration}
               onChange={(e) => handleChange(index, "duration", e.target.value)}
-              className="w-full mt-1 p-2 border rounded-full"
+              className={`w-full mt-1 p-2 border rounded-full ${
+                isDarkMode ? "bg-gray-700 text-white" : "bg-gray-100 text-black"
+              }`}
             />
           </div>
         ))}
         <div className="flex justify-end space-x-4">
           <button
             onClick={onClose}
-            className="px-4 py-2 rounded-full bg-gray-400 text-white hover:bg-gray-500"
+            className={`px-4 py-2 rounded-full ${
+              isDarkMode
+                ? "bg-gray-600 text-white hover:bg-gray-500"
+                : "bg-gray-400 text-black hover:bg-gray-500"
+            }`}
           >
             Cancel
           </button>
           <button
             onClick={handleSave}
-            className="px-4 py-2 rounded-full bg-blue-500 text-white hover:bg-blue-600"
+            className={`px-4 py-2 rounded-full ${
+              isDarkMode
+                ? "bg-blue-500 text-white hover:bg-blue-600"
+                : "bg-blue-500 text-white hover:bg-blue-600"
+            }`}
           >
             Save
           </button>
@@ -48,7 +64,5 @@ const SettingsModal = ({ tabsData, onSave, onClose }) => {
     </div>
   );
 };
-
-
 
 export default SettingsModal;
